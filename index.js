9c9be7bee0e9576fbb291f10dc62f29a43f3b55d
Mihocsa Szilard <mihocsa48@gmail.com>
2021-07-16 08:50:14 +0200

    Revert "console.log actorID"
    This reverts commit b92d8188804eab43e85e07c411e6af7beb5cba96.

--- index.js
+++ index.js
@@ -438,7 +438,6 @@ app.get('/movies/:ActorID', passport.authenticate('jwt', {
   })
   .then((movie) => {
     res.json(movie);
-    console.log(req.params.ActorID);
   }).catch((err) => {
     console.error(err);
     res.status(500).send('Error: ' + err);
