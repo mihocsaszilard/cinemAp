dbb99388a1a81ab6e9269a4daf572fd0d9b09a84
Mihocsa Szilard <mihocsa48@gmail.com>
2021-07-26 12:15:36 +0200

    added jwt auth

--- index.js
+++ index.js
@@ -62,7 +62,9 @@ app.get('/', (req, res) => {
 
 //------------------movie requests---------------
 //get all movies
-app.get('/movies', function(req, res) {
+app.get('/movies', passport.authenticate('jwt', {
+  session: false
+}), function(req, res) {
   Movies.find()
   .populate('Genre Director Actors', 'Name')
     .then((movies) => {
@@ -199,7 +201,9 @@ app.delete('/users/:Username/removeFromFav/:MovieID', passport.authenticate('jwt
 
 //----------------genre requests--------------------
 //get all genres
-app.get('/genres', function(req, res) {
+app.get('/genres', passport.authenticate('jwt', {
+  session: false
+}), function(req, res) {
   Genres.find()
     .then((genre) => {
       res.status(200).json(genre);
@@ -226,7 +230,9 @@ app.get('/genres/:Name', passport.authenticate('jwt', {
 
 //----------------director requests--------------------
 //get all directors
-app.get('/directors', function(req, res) {
+app.get('/directors', passport.authenticate('jwt', {
+  session: false
+}), function(req, res) {
   Directors.find()
     .then((director) => {
       res.status(200).json(director);
@@ -253,7 +259,9 @@ app.get('/directors/:Name', passport.authenticate('jwt', {
 
 //------------------user requests---------------
 //gett all users
-app.get('/users', function(req, res) {
+app.get('/users', passport.authenticate('jwt', {
+  session: false
+}), function(req, res) {
   Users.find()
     .then((users) => {
       res.status(201).json(users);
