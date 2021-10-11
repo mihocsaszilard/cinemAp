e01e53cd4e836406e17226e06caeba8075b6c2d9
Mihocsa Szilard <mihocsa48@gmail.com>
2021-10-11 12:01:51 +0200

    allowedOrigins: *

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const mongoose = require("mongoose"),
 const cors = require("cors");
 
 let allowedOrigins = [
+  "*",
   "http://localhost:8080",
   "https://cinemapp-backend.herokuapp.com/",
   "http://localhost:1234",
