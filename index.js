db8c992b67a86b9ba539cda377b7efbbb5ad7f59
Mihocsa Szilard <mihocsa48@gmail.com>
2021-10-15 10:56:02 +0200

    added angular ghPages to allowed origins

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let allowedOrigins = [
   "http://localhost:1234",
   "http://localhost:4200",
   "https://mihocsaszilard-cinemapp.netlify.app",
-  "https://mihocsaszilard.github.io/cinemApp-Angular-client",
+  "https://mihocsaszilard.github.io/",
 ];
 
 app.use(
